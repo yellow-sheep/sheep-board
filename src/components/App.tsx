b077e2bd116ad53c91449efe122a905aca6264a2
joeyw <jjay9225@gmail.com>
2019-02-16 10:35:13 -0500

    Test change

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,7 +10,7 @@ namespace Layers {
     <Toggle.Consumer>
       {({ on }) => (
         <>
-          {on ? 'The button is on' : 'The button is off'}
+          {on ? 'The button is on!!' : 'The button is off!!'}
           <Layer3 />
         </>
       )}
